e8454d0eda696a55594483157bbadf2f9bbb9f10
Cristian <cristian.bautista@sportnco.com>
2018-08-09 15:35:03 +0200

    Modigicar

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
-<?php
+s<?php
 $i = 0
-if($i==0){
-echo "prueba";
+if($i==0){daasdasdasdasdasdasdasdasdasd
+echo "prueba";sasdasdasdasdasdads
 }
-for($i=0){
+for($i=0){dasdasdasdadasdasdasdasdasdasdasd
 }
+
+dadasdasdasdasdasdasdasdasdasda
 ?>
